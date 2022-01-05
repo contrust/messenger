050e4976ffbacded5d6987b45fb2e4169f8884e5
contrust <2001raf@gmail.com>
2022-01-05 17:43:55 +0500

    russian to english translator message handler added

---
+++ Messenger.Application/MessageHandlers/TextConverter/RussianToEnglishTranslator/RussianToEnglishTranslator.cs
@@ -0,0 +1,47 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Messenger.Application.MessageHandlers
+{
+    public static class RussianToEnglishTranslator
+    {
+        public static string Translate(string sourceText)
+        {
+            string englishTranslation;
+            try
+            {
+                string googleJsonTranslation;
+                var url =
+                    $"https://translate.googleapis.com/translate_a/single?client=gtx&" +
+                    $"sl=ru&" +
+                    $"tl=en&" +
+                    $"dt=t&q={HttpUtility.UrlEncode(sourceText)}";
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers.Add("user-agent",
+                        "Mozilla/5.0 (Windows NT 6.1) " +
+                        "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                        "Chrome/41.0.2228.0 Safari/537.36");
+                    wc.Encoding = System.Text.Encoding.UTF8;
+                    googleJsonTranslation = wc.DownloadString(url);
+                }
+                
+                var json = JsonConvert.DeserializeObject(googleJsonTranslation);
+                englishTranslation = ((((json as JArray).FirstOrDefault() as JArray)
+                        .FirstOrDefault() as JArray)
+                        .FirstOrDefault() as JValue)?
+                    .Value
+                    .ToString();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            return englishTranslation;
+        }
+    }
+}
